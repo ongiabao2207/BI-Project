0e84bd57802cd53030d5b6c8bbaa1f3d28317859
ongiabao2207 <ongiabao36@gmail.com>
2025-11-07 23:33:46 +0700

    Create Metadata database

---
+++ ScriptDB/Metadata_DB.sql
@@ -0,0 +1,23 @@
+﻿use master
+DROP DATABASE IF EXISTS Project_Metadata_DB;
+go
+
+create database Project_Metadata_DB
+go
+use Project_Metadata_DB
+go
+
+
+--DROP TABLE ETL_Metadata
+CREATE TABLE ETL_Metadata(
+  SourceName VARCHAR(50) PRIMARY KEY,
+  LSET DATETIME NOT NULL DEFAULT '1900-01-01' -- initial value    
+);
+
+INSERT INTO ETL_Metadata (SourceName) VALUES
+    ('Airline'),
+    ('Airport'),
+    ('Flight');
+
+
+select * from ETL_Metadata;
